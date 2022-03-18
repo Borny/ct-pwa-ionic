c9b301e52e94dfe4899e6aadb770967cff8ab1db
tristan <tristan@citytaps.org>
2022-03-18 10:21:57 +0100

    Init

---
+++ src/components/ExploreContainer.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="container">
+    <p>
+      <strong>Mat Bond</strong>
+    </p>
+    <p>1234 5678 90</p>
+
+    <div class="icon">
+      <ion-icon :icon="water"></ion-icon>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { IonIcon } from "@ionic/vue";
+import { defineComponent } from "vue";
+import { water } from "ionicons/icons";
+export default defineComponent({
+  name: "ExploreContainer",
+  props: {
+    name: String,
+  },
+  components: {
+    IonIcon,
+  },
+  setup() {
+    return {
+      water,
+    };
+  },
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+
+.icon {
+  font-size: 3rem;
+  color: blue;
+}
+</style>
---
+++ src/views/Tab3Page.vue
@@ -0,0 +1,43 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button menu="main"></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Consumption</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ExploreContainer name="Consumption" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonMenuButton,
+} from "@ionic/vue";
+import ExploreContainer from "@/components/ExploreContainer.vue";
+
+export default defineComponent({
+  name: "Tab3Page",
+  components: {
+    ExploreContainer,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonButtons,
+    IonMenuButton,
+  },
+});
+</script>
