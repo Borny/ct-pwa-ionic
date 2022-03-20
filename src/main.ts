ebc4a6edd423cc9aa29a07e3ef7de35f95f78c07
tristan <tristan@citytaps.org>
2022-03-20 21:11:31 +0100

    pwa working

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
@@ -9,6 +9,7 @@ import '@ionic/vue/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
 import '@ionic/vue/css/normalize.css';
+
 import '@ionic/vue/css/structure.css';
 import '@ionic/vue/css/typography.css';
 
@@ -20,13 +21,13 @@ import '@ionic/vue/css/text-transformation.css';
 import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
 
+import './registerServiceWorker';
+
 /* Theme variables */
 import './theme/variables.css';
 
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-  
+const app = createApp(App).use(IonicVue).use(router);
+
 router.isReady().then(() => {
   app.mount('#app');
 });
